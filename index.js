124e3d76ce3d4dc62235cb715394d305671b58db
nichoguimbaolibot <nichoguimbaolibot@gmail.com>
2018-12-30 18:35:22 +0800

    initalization setup:

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+const MyName = () => 
+  <div>
+    <h1>Nicho Guimbaolibot</h1>
+    <h3>Software Engineer</h3>
+  </div>
+
+
+ReactDOM.render(<MyName />, document.getElementById("root"))
